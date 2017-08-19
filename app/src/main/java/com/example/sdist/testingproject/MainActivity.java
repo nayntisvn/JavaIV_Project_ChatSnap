a77cc80636f7948c527bdaa374d93ebbb3de73bf
--null <=>
2017-08-19 22:17:22 +0800

    Changes

--- app/src/main/java/com/example/sdist/testingproject/MainActivity.java
+++ app/src/main/java/com/example/sdist/testingproject/MainActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void putanginamo(){
-        String lavea;
-    }
+
+
 }
