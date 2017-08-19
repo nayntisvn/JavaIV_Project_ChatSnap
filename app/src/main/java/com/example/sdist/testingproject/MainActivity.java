2ec83276afbd0c2399823bc7943b34871fb432ee
hjacynnbenitez <hjacynn.benitez@gmail.com>
2017-08-20 00:11:43 +0800

    ui update

--- app/src/main/java/com/example/sdist/testingproject/MainActivity.java
+++ app/src/main/java/com/example/sdist/testingproject/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.sdist.testingproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.root_layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(500);
+        animationDrawable.start();
     }
 
     public void putanginamo(){
