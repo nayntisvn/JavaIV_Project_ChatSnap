eda1ae9fe9d241d9bcdc671f3d0a0ba9c5a2b0ee
Ezekiel Flores <ezekieljoshuaf@gmail.com>
2017-09-10 13:27:53 +0800

    Push

--- app/src/main/java/com/example/sdist/testingproject/friendlist.java
+++ app/src/main/java/com/example/sdist/testingproject/friendlist.java
@@ -1,11 +1,18 @@
 package com.example.sdist.testingproject;
 
+import android.content.Context;
 import android.net.ParseException;
 import android.os.AsyncTask;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.text.format.DateFormat;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,8 +26,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class friendlist extends AppCompatActivity {
+    public class mAdapter extends ArrayAdapter<Friend>{
 
-    ArrayAdapter<Friend> adapter;
+        public mAdapter(@NonNull Context context, ArrayList<Friend> objects) {
+            super(context, 0, objects);
+        }
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            ViewHolder viewHolder;
+            if (view == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.friend, viewGroup, false);
+
+                viewHolder = new ViewHolder();
+
+                viewHolder.textView = (TextView) view.findViewById(R.id.friend);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            Friend myModel = getItem(position);
+
+            viewHolder.textView.setText(myModel.getFriendName());
+
+
+            return view;
+        }
+        public class ViewHolder {
+            TextView textView;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,17 +80,19 @@ public class friendlist extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        adapter = new ArrayAdapter<Friend>(friendlist.this,
-                R.layout.friend, listFriends) {
+//        adapter = new ArrayAdapter<Friend>(friendlist.this,
+//                R.layout.friend, listFriends) {
+//
+//            protected void getView(int position, View v, Friend model) {
+//                // Get references to the views of message.xml
+//                TextView friendText = (TextView)v.findViewById(R.id.friend);
+//
+//                // Set their text
+//                friendText.setText(model.getFriendName());
+//            }
+//        };
 
-            protected void getView(int position, View v, Friend model) {
-                // Get references to the views of message.xml
-                TextView friendText = (TextView)v.findViewById(R.id.friend);
-
-                // Set their text
-                friendText.setText(model.getFriendName());
-            }
-        };
+        mAdapter adapter = new mAdapter(friendlist.this, listFriends);
         listOfFriends.setAdapter(adapter);
     }
 
